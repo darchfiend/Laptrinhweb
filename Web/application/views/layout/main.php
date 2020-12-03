9a882e9e2ab290c5c73ec91cd7cf69f69ed785b4
= <vui.trinhtk@gmail.com>
2020-12-03 19:35:38 +0700

    Updated 7pm 03/12/20

---
+++ Web/application/views/layout/main.php
@@ -0,0 +1,5 @@
+<div class="col-sm-10">
+	<h2>Tin mới nhất</h2>
+	<?php
+	?>
+</div>
---
+++ Web/application/views/layout/sidebar.php
@@ -0,0 +1,41 @@
+
+	<div class="col-sm-2">
+		<h5>Chọn thể loại</h5>
+		<form method="post" action="">
+	<select name="feedurl" id="danhmuc">
+		<option value="https://vnexpress.net/rss/tin-moi-nhat.rss">Tin mới nhất</option>
+		<option value="https://vnexpress.net/rss/chinh-tri.rss">Chính trị</option>
+		<option value="https://vnexpress.net/rss/giao-duc.rss">Giáo dục</option>
+		<option value="https://vnexpress.net/rss/the-thao.rss">Thể thao</option>
+		<option value="https://vnexpress.net/rss/van-hoa.rss">Văn hóa</option>
+		<option value="https://vnexpress.net/rss/kinh-doanh.rss">Kinh doanh</option>
+		<option value="https://vnexpress.net/rss/phap-luat.rss">Pháp luật</option>
+	</select>><input type="submit" name="submit" value='Chọn'>
+</form>
+
+	   	<h5>Danh mục</h5>
+	   	<ul class="nav nav-pills flex-column">
+	    	<li class="nav-item"><a  class="nav-link active" href="#">Trang chủ</a></li>
+	    	<li class="nav-item dropdown active">
+		        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" >Tin tức mới trong tuần</a> 
+		        <ul class="dropdown-menu" >
+		            <li class="nav-item"><a class="nav-link" href="#" >Chính trị</a> </li>
+					<li class="nav-item"><a class="nav-link" href="#" >Giáo dục</a> </li>
+			        <li class="nav-item"><a class="nav-link" href="#" >Thể thao</a> </li>
+			        <li class="nav-item"><a class="nav-link" href="#" >Văn hóa</a> </li>
+					<li class="nav-item"><a class="nav-link" href="#" >Kinh doanh</a> </li>
+					<li class="nav-item"><a class="nav-link" href="#" >Pháp luật</a> </li>
+	        	</ul>
+	      	</li>
+	      	<li class="nav-item dropdown active">
+		        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Về chúng tôi</a> 
+		        <ul class="dropdown-menu" >
+		            <li class="nav-item"><a class="nav-link" href="#" >1851010034 - Đỗ Trọng Đức</a> </li>
+		            <li class="nav-item"><a class="nav-link" href="#" >1851010157 - Trịnh Thị Kim Vui</a> </li>
+		            <li class="nav-item"><a class="nav-link" href="#" >1851010104 - Nguyễn Hoàng Phúc</a> </li>
+		        </ul>
+	       	</li>
+	       	<li class="nav-item"><a class="nav-link" href="#" >Tổng số lượt truy cập</a> </li>
+	    </ul>
+	</div>
+
